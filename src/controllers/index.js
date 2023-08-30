a61bccd2554fdf5311ccc905f42e00a59d19f1b9
Bryan <alanm.ceron@gmail.com>
2023-08-30 12:20:54 -0600

    NuevaApi

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,10 +1,10 @@
 
 
-exports.getResultado = (req,res)=>{
+exports.getResultado = (req, res) => {
 
-    const { fechaInicio, fechaFin, empleado} = req.query;
+    const { fechaInicio, fechaFin, empleado } = req.query;
 
-    req.getConnection((err,conn)=>{
+    req.getConnection((err, conn) => {
         if (err) return res.send(err);
 
 
@@ -22,21 +22,21 @@ exports.getResultado = (req,res)=>{
     AND B.Nombre LIKE ? 
 
     GROUP BY B.Nombre, D.clave, C.descripcion
-    ORDER BY B.Nombre, D.clave`, [fechaInicio, fechaFin, empleado], (err,result)=>{
+    ORDER BY B.Nombre, D.clave`, [fechaInicio, fechaFin, empleado], (err, result) => {
             if (err) return res.send(err);
             res.send(result);
 
         })
     }
-  
+
     )
 }
 
-exports.getTotalPri = (req,res)=>{
+exports.getTotalPri = (req, res) => {
 
-    const { fechaInicio, fechaFin} = req.query;
+    const { fechaInicio, fechaFin } = req.query;
 
-    req.getConnection((err,conn)=>{
+    req.getConnection((err, conn) => {
         if (err) return res.send(err);
 
 
@@ -52,15 +52,14 @@ exports.getTotalPri = (req,res)=>{
     INNER JOIN serviciotiporeporte D ON A.serviciotiporeporte_id = D.id 
     WHERE A.fecha BETWEEN ? AND ?
      
-
     GROUP BY B.Nombre, D.clave, C.descripcion
-    ORDER BY B.Nombre, D.clave`, [fechaInicio, fechaFin], (err,result)=>{
+    ORDER BY B.Nombre, D.clave`, [fechaInicio, fechaFin], (err, result) => {
             if (err) return res.send(err);
             res.send(result);
 
         })
     }
-  
+
     )
 }
 
@@ -85,6 +84,104 @@ exports.getTabla = (req, res) => {
             if (err) return res.send(err);
             res.send(result);
         });
+    });
+};
+
+
+exports.getGrafica = (req, res) => {
+    const { fechaInicio, fechaFin } = req.query;
+
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err);
+
+        const hechosEnForma = `
+        SELECT 
+            COUNT(*) AS Cantidad
+        FROM servicioreporte A
+        INNER JOIN empleado B ON A.responsable_empleado_id = B.Id
+        INNER JOIN servicioprioridad C ON A.prioridad = C.id
+        INNER JOIN serviciotiporeporte D ON A.serviciotiporeporte_id = D.id 
+        WHERE A.fecha BETWEEN ? AND ?  AND
+        CONCAT(A.fecha_solucion, ' ', A.horasolucion) <= CONCAT(A.fecha, ' ', A.hora) + INTERVAL C.hrs HOUR`;
+
+        const hechosVencidos = `
+        SELECT 
+            COUNT(*) AS Cantidad
+        FROM servicioreporte A
+        INNER JOIN empleado B ON A.responsable_empleado_id = B.Id
+        INNER JOIN servicioprioridad C ON A.prioridad = C.id
+        INNER JOIN serviciotiporeporte D ON A.serviciotiporeporte_id = D.id 
+        WHERE A.fecha BETWEEN ? AND ?  AND
+        CONCAT(A.fecha_solucion, ' ', A.horasolucion) >= CONCAT(A.fecha, ' ', A.hora) + INTERVAL C.hrs HOUR`;
+
+
+        const pendientesVencidos = `
+        SELECT 
+
+        COUNT(*) AS cantidad_registros
+         FROM servicioreporte A
+        INNER JOIN servicioprioridad B ON A.prioridad = B.id
+        WHERE fecha BETWEEN '2023-05-31' AND '2023-08-03' AND
+        activo = 1
+        AND CONCAT(A.fecha, ' ', A.hora) + INTERVAL B.hrs HOUR < now() `;
+ 
+
+const pendientesNoVencidos = `
+SELECT 
+
+COUNT(*) AS cantidad_registros
+ FROM servicioreporte A
+INNER JOIN servicioprioridad B ON A.prioridad = B.id
+WHERE fecha BETWEEN '2023-05-31' AND '2023-08-03' AND
+activo = 1
+AND CONCAT(A.fecha, ' ', A.hora) + INTERVAL B.hrs HOUR > now()`
+
+
+
+        conn.query(
+            hechosEnForma,
+            [fechaInicio, fechaFin],
+            (err, hechosEnFormaResult) => {
+                if (err) return res.send(err);
+
+                conn.query(
+                    hechosVencidos,
+                    [fechaInicio, fechaFin],
+                    (err, hechosVencidosResult) => {
+                        if (err) return res.send(err);
+
+                        conn.query(
+                            pendientesVencidos,
+                            [fechaInicio, fechaFin],
+                            (err, pendientesVencidosResult) => {
+                                if (err) return res.send(err);
+
+                                conn.query(
+                                    pendientesNoVencidos,
+                                    [fechaInicio, fechaFin],
+                                    (err, pendientesNoVencidosResult) => {
+                                        if (err) return res.send(err);
+
+
+                        const combinedResults = {
+                            hechosEnForma: hechosEnFormaResult,
+                            hechosVencidos: hechosVencidosResult,
+                            pendientesVencidos:pendientesVencidosResult,
+                            pendientesNoVencidos:pendientesNoVencidosResult
+
+                        };
+
+                        res.send(combinedResults);
+                    }
+                );
+            }
+            );
+
+        }
+        );
+
+            }
+        );
     });
 };
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const route = express.Router();
-const { getTabla, getResultado, getTotalPri} = require("../controllers/index")
+const { getTabla, getResultado, getTotalPri, getGrafica } = require("../controllers/index")
 
 
 
 route.get("/tabla", getTabla);
 route.get("/resultado", getResultado);
 route.get("/prioridad", getTotalPri);
+route.get("/grafica", getGrafica);
 
 
 module.exports = route;
