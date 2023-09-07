525344d147bfc811e7d7a095ff0ef5f95e7ef676
Bryan <alanm.ceron@gmail.com>
2023-09-07 09:23:10 -0600

    NuevasConsultas

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -148,29 +148,38 @@ exports.getGrafica = (req, res) => {
             
                     const porVencer = `
                     SELECT 
-                    COUNT(*) AS cantidad_registros_por_vencer
-                FROM servicioreporte A
-                LEFT JOIN servicioprioridad B ON A.prioridad = B.id
-                WHERE activo = 1
-                AND (CONCAT(A.fecha, ' ', A.hora) + INTERVAL B.hrs / 2 HOUR) > NOW()
-                AND (CONCAT(A.fecha, ' ', A.hora) + INTERVAL B.hrs HOUR) > NOW();`;
+    COUNT(*) AS cantidad_registros_por_vencer
+FROM servicioreporte A
+ LEFT JOIN empleado B ON A.responsable_empleado_id = B.Id
+        LEFT JOIN servicioprioridad C ON A.prioridad = C.id
+        LEFT JOIN serviciotiporeporte D ON A.serviciotiporeporte_id = D.id 
+WHERE A.activo = 1 AND
+ D.clave IS NOT NULL
+AND (CONCAT(A.fecha, ' ', A.hora) + INTERVAL C.hrs / 2 HOUR) > NOW()
+AND (CONCAT(A.fecha, ' ', A.hora) + INTERVAL C.hrs HOUR) > NOW();`;
             
                     const aTiempo = `
                     SELECT 
-                    COUNT(*) AS cantidad_registros_por_vencer
-                FROM servicioreporte A
-                LEFT JOIN servicioprioridad B ON A.prioridad = B.id
-                WHERE activo = 1
-                AND (CONCAT(A.fecha, ' ', A.hora) + INTERVAL B.hrs / 2 HOUR) <= NOW()
-                AND (CONCAT(A.fecha, ' ', A.hora) + INTERVAL B.hrs HOUR) > NOW();`;
+    COUNT(*) AS cantidad_registros_por_vencer
+FROM servicioreporte A
+ LEFT JOIN empleado B ON A.responsable_empleado_id = B.Id
+        LEFT JOIN servicioprioridad C ON A.prioridad = C.id
+        LEFT JOIN serviciotiporeporte D ON A.serviciotiporeporte_id = D.id 
+WHERE A.activo = 1 AND
+ D.clave IS NOT NULL
+AND (CONCAT(A.fecha, ' ', A.hora) + INTERVAL C.hrs / 2 HOUR) <= NOW()
+AND (CONCAT(A.fecha, ' ', A.hora) + INTERVAL C.hrs HOUR) > NOW();`;
 
                 const vencido = `
                 SELECT 
-                COUNT(*) AS cantidad_registros
-                         FROM servicioreporte A
-                        LEFT JOIN servicioprioridad B ON A.prioridad = B.id
-                        WHERE activo = 1
-                        AND CONCAT(A.fecha, ' ', A.hora) + INTERVAL B.hrs HOUR < now() ;`;
+                COUNT(*) AS cantidad_registros_por_vencer
+            FROM servicioreporte A
+             LEFT JOIN empleado B ON A.responsable_empleado_id = B.Id
+                    LEFT JOIN servicioprioridad C ON A.prioridad = C.id
+                    LEFT JOIN serviciotiporeporte D ON A.serviciotiporeporte_id = D.id 
+            WHERE A.activo = 1 AND
+             D.clave IS NOT NULL
+                    AND CONCAT(A.fecha, ' ', A.hora) + INTERVAL C.hrs HOUR < now() ;`;
 
                     conn.query(
                         porVencer,
